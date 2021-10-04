92395b3e6a9a709a6f9ffa519a345cc965d36a6c
Alassane Ndoye <aloutndoye.an@gmail.com>
2021-10-04 21:26:37 +0000

    Interior Page Template

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -10,3 +10,8 @@
 	}
 
 	add_action('wp_enqueue_scripts', 'assets_files');
+
+	function university_features(){
+		add_theme_support('title-tag');
+}
+	add_action('after_setup_theme', 'university_features');
--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -3,11 +3,82 @@
 
     while (have_posts()){
         the_post(); ?>
-        <h2><?php the_title() ?></h2>
-        <?php the_content(); ?>
-        <hr/>
+
+        <div class="page-banner">
+            <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?> )"></div>
+            <div class="page-banner__content container container--narrow">
+                <h1 class="page-banner__title"><?php the_title() ?></h1>
+                <div class="page-banner__intro">
+                    <p>DONT FORGET TO REPLACE ME LATER</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="container container--narrow page-section">
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <p>
+                    <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
+                </p>
+            </div>
+<!--
+            <div class="page-links">
+                <h2 class="page-links__title"><a href="#">About Us</a></h2>
+                <ul class="min-list">
+                    <li class="current_page_item"><a href="#">Our History</a></li>
+                    <li><a href="#">Our Goals</a></li>
+                </ul>
+            </div>
+-->
+            <div class="generic-content">
+                <?php the_content(); ?>
+\            </div>
+        </div>
+
+        <div class="page-section page-section--beige">
+            <div class="container container--narrow generic-content">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
+            </div>
+        </div>
+
+        <div class="page-section page-section--white">
+            <div class="container container--narrow">
+                <h2 class="headline headline--medium">Biology Professors:</h2>
+
+                <ul class="professor-cards">
+                    <li class="professor-card__list-item">
+                        <a href="#" class="professor-card">
+                            <img class="professor-card__image" src="<?php echo get_theme_file_uri('/images/barksalot.jpg') ?>" />
+                            <span class="professor-card__name">Dr. Barksalot</span>
+                        </a>
+                    </li>
+                    <li class="professor-card__list-item">
+                        <a href="#" class="professor-card">
+                            <img class="professor-card__image" src="<?php echo get_theme_file_uri('/images/meowsalot.jpg') ?>" />
+                            <span class="professor-card__name">Dr. Meowsalot</span>
+                        </a>
+                    </li>
+                </ul>
+                <hr class="section-break" />
+
+                <div class="row group generic-content">
+                    <div class="one-third">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
+                    </div>
+
+                    <div class="one-third">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
+                    </div>
+
+                    <div class="one-third">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <?php
     }
-
     get_footer();
 ?>
