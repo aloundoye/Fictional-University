85f7c74b124f47a2226a7d8fdc1ce3d2eb7d494b
Alassane Ndoye <aloutndoye.an@gmail.com>
2021-10-04 20:38:34 +0000

    convert static HTML template into wordpress
    Template code: https://github.com/LearnWebCode/university-static

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -1,5 +1,11 @@
 <?php
 	function assets_files(){
+		wp_enqueue_script('main-university-js', get_theme_file_uri('/js/script-bundled.js'), NULL,
+			'1.0', true);
+		wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,
+		400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+		wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+		wp_enqueue_style('index', get_stylesheet_directory_uri().'/index.css', false, null);
 		wp_enqueue_style('university_main_styles', get_stylesheet_uri());
 	}
 
