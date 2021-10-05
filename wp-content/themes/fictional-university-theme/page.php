d9b3b84f435ea312dd36adb6c8bac56961b5e1fb
Alassane Ndoye <aloutndoye.an@gmail.com>
2021-10-05 18:18:08 +0000

    Parent and children pages

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -1,84 +1,113 @@
 <?php
-    get_header();
+get_header();
 
-    while (have_posts()){
-        the_post(); ?>
+while ( have_posts() ) {
+	the_post(); ?>
 
-        <div class="page-banner">
-            <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?> )"></div>
-            <div class="page-banner__content container container--narrow">
-                <h1 class="page-banner__title"><?php the_title() ?></h1>
-                <div class="page-banner__intro">
-                    <p>DONT FORGET TO REPLACE ME LATER</p>
-                </div>
+    <div class="page-banner">
+        <div class="page-banner__bg-image"
+             style="background-image: url(<?php echo get_theme_file_uri( '/images/ocean.jpg' ) ?> )"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php the_title() ?></h1>
+            <div class="page-banner__intro">
+                <p>DONT FORGET TO REPLACE ME LATER</p>
             </div>
         </div>
+    </div>
 
-        <div class="container container--narrow page-section">
+    <div class="container container--narrow page-section">
+		<?php
+        $parent= wp_get_post_parent_id( get_the_ID() );
+        if ($parent) { ?>
             <div class="metabox metabox--position-up metabox--with-home-link">
                 <p>
-                    <a class="metabox__blog-home-link" href="<?php site_url('/about-us');?>"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main"><?php the_title() ?></span>
+                    <a class="metabox__blog-home-link" href="<?php echo get_permalink($parent) ?>"><i
+                                class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($parent) ?></a> <span
+                            class="metabox__main"><?php the_title() ?></span>
                 </p>
             </div>
-<!--
-            <div class="page-links">
-                <h2 class="page-links__title"><a href="#">About Us</a></h2>
-                <ul class="min-list">
-                    <li class="current_page_item"><a href="#">Our History</a></li>
-                    <li><a href="#">Our Goals</a></li>
-                </ul>
-            </div>
--->
-            <div class="generic-content">
-                <?php the_content(); ?>
-\            </div>
+		<?php } ?>
+        <!--
+					<div class="page-links">
+						<h2 class="page-links__title"><a href="#">About Us</a></h2>
+						<ul class="min-list">
+							<li class="current_page_item"><a href="#">Our History</a></li>
+							<li><a href="#">Our Goals</a></li>
+						</ul>
+					</div>
+		-->
+        <div class="generic-content">
+			<?php the_content(); ?>
         </div>
+    </div>
 
-        <div class="page-section page-section--beige">
-            <div class="container container--narrow generic-content">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
+    <div class="page-section page-section--beige">
+        <div class="container container--narrow generic-content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid
+                possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis,
+                consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora
+                alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea
+                veniam, ad quod sed.</p>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
-            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid
+                possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis,
+                consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora
+                alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea
+                veniam, ad quod sed.</p>
         </div>
+    </div>
 
-        <div class="page-section page-section--white">
-            <div class="container container--narrow">
-                <h2 class="headline headline--medium">Biology Professors:</h2>
+    <div class="page-section page-section--white">
+        <div class="container container--narrow">
+            <h2 class="headline headline--medium">Biology Professors:</h2>
 
-                <ul class="professor-cards">
-                    <li class="professor-card__list-item">
-                        <a href="#" class="professor-card">
-                            <img class="professor-card__image" src="<?php echo get_theme_file_uri('/images/barksalot.jpg') ?>" />
-                            <span class="professor-card__name">Dr. Barksalot</span>
-                        </a>
-                    </li>
-                    <li class="professor-card__list-item">
-                        <a href="#" class="professor-card">
-                            <img class="professor-card__image" src="<?php echo get_theme_file_uri('/images/meowsalot.jpg') ?>" />
-                            <span class="professor-card__name">Dr. Meowsalot</span>
-                        </a>
-                    </li>
-                </ul>
-                <hr class="section-break" />
+            <ul class="professor-cards">
+                <li class="professor-card__list-item">
+                    <a href="#" class="professor-card">
+                        <img class="professor-card__image"
+                             src="<?php echo get_theme_file_uri( '/images/barksalot.jpg' ) ?>"/>
+                        <span class="professor-card__name">Dr. Barksalot</span>
+                    </a>
+                </li>
+                <li class="professor-card__list-item">
+                    <a href="#" class="professor-card">
+                        <img class="professor-card__image"
+                             src="<?php echo get_theme_file_uri( '/images/meowsalot.jpg' ) ?>"/>
+                        <span class="professor-card__name">Dr. Meowsalot</span>
+                    </a>
+                </li>
+            </ul>
+            <hr class="section-break"/>
 
-                <div class="row group generic-content">
-                    <div class="one-third">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
-                    </div>
+            <div class="row group generic-content">
+                <div class="one-third">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus
+                        aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit
+                        illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio
+                        repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
+                </div>
 
-                    <div class="one-third">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
-                    </div>
+                <div class="one-third">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus
+                        aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit
+                        illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio
+                        repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
+                </div>
 
-                    <div class="one-third">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
-                    </div>
+                <div class="one-third">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus
+                        aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit
+                        illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio
+                        repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
                 </div>
             </div>
         </div>
+    </div>
 
-        <?php
-    }
-    get_footer();
+	<?php
+}
+get_footer();
 ?>
