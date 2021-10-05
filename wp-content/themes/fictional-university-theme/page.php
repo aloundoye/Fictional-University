882ac310707e148d52e0d1e205de616b2245fa34
Alassane Ndoye <aloutndoye.an@gmail.com>
2021-10-05 19:34:32 +0000

    Menu of child pages links

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -27,15 +27,32 @@ while ( have_posts() ) {
                 </p>
             </div>
 		<?php } ?>
-        <!--
+        <?php
+
+        $hasChild = get_pages(array(
+                'child_of' => get_the_ID()
+        ));
+            if ($parent or $hasChild){
+
+        ?>
 					<div class="page-links">
-						<h2 class="page-links__title"><a href="#">About Us</a></h2>
+						<h2 class="page-links__title"><a href="<?php echo get_permalink($parent) ?>"><?php echo get_the_title($parent) ?></a></h2>
 						<ul class="min-list">
-							<li class="current_page_item"><a href="#">Our History</a></li>
-							<li><a href="#">Our Goals</a></li>
+							<?php
+							if ($parent){
+								$findChildrenOf = $parent;
+							}else{
+								$findChildrenOf = get_the_ID();
+							}
+                                wp_list_pages(array(
+	                                'title_li' => 0,
+                                    'child_of' => $findChildrenOf,
+                                    'sort_column' => 'menu_order'
+                                ));
+                            ?>
 						</ul>
 					</div>
-		-->
+        <?php } ?>
         <div class="generic-content">
 			<?php the_content(); ?>
         </div>
