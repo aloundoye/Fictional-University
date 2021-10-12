cf2ad5413d0d1a028c55aa847a5b6169cb86e9c2
Alassane Ndoye <aloutndoye.an@gmail.com>
2021-10-12 23:47:22 +0000

    Update custom post event

--- wp-content/mu-plugins/university_post_types.php
+++ wp-content/mu-plugins/university_post_types.php
@@ -2,6 +2,7 @@
 	//callback function for adding post types
 	function university_post_types(){
 		register_post_type('event', array(
+			'supports' => array('title', 'editor', 'excerpt'),
 			'rewrite' => array('slug' => 'events'),
 			'has_archive' => true,
 			'public' => true,
