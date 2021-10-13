f6205fb0980539577945266a30fed312719b000c
Alassane Ndoye <aloutndoye.an@gmail.com>
2021-10-13 18:32:27 +0000

    ordering events in home page

--- wp-content/themes/fictional-university-theme/front-page.php
+++ wp-content/themes/fictional-university-theme/front-page.php
@@ -17,12 +17,25 @@ get_header();
 		<div class="full-width-split__inner">
 			<h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
                 <?php
+                //custom query for events
+                    $today = date('Ymd');
                     $homepageEvents = new WP_Query(array(
-                            'posts_per_page' => 2,
+                            'posts_per_page' => -1,
                             'post_type' => 'event',
+                            'meta_key' => 'event_date',
+                            'orderby' => 'meta_value_num',
+                            'order' => 'ASC',
+                            'meta_query' => array(
+                              array(
+                                  'key' => 'event_date',
+                                  'compare'=> '>=',
+                                  'value' => $today,
+                                  'type' => 'numeric'
+                              )
+                            )
                         )
                     );
-
+                    //retrieve event post
                     while($homepageEvents->have_posts()){
                         $homepageEvents->the_post();?>
                     <div class="event-summary">
