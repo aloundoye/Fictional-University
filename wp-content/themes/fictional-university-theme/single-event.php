38e5c245aaeeeb9c3317e6fa05a9b2da9746f16c
Alassane Ndoye <aloutndoye.an@gmail.com>
2021-10-15 17:48:22 +0000

    Display the relation between program and event

--- wp-content/themes/fictional-university-theme/single-event.php
+++ wp-content/themes/fictional-university-theme/single-event.php
@@ -24,6 +24,21 @@ while (have_posts()){
 			</p>
 		</div>
 		<div class="generic-content"><?php the_content();?></div>
+        <!--Display related programs for an event-->
+        <?php
+            $relatedPrograms = get_field('related_programs');
+            if ($relatedPrograms){
+	            echo '<hr class="section-break">';
+	            echo '<h2 class="headline headline--medium">Related Program(s)</h2>';
+	            echo '<ul class ="link-list min-list">';
+	            foreach ($relatedPrograms as $program){?>
+                    <li><a href="<?php echo get_the_permalink($program);?>"><?php echo get_the_title($program); ?></a></li>
+		            <?php
+	            }
+	            echo '</ul>';
+            }
+
+        ?>
 	</div>
 	<?php
 }
--- wp-content/themes/fictional-university-theme/single-program.php
+++ wp-content/themes/fictional-university-theme/single-program.php
@@ -24,6 +24,72 @@ while (have_posts()){
             </p>
         </div>
         <div class="generic-content"><?php the_content();?></div>
+	    <?php
+            //custom query for retrieving events related to the current program
+            $today = date('Ymd');
+            $relatedEvents = new WP_Query(array(
+                    'posts_per_page' => 2,
+                    'post_type' => 'event',
+                    'meta_key' => 'event_date',
+                    'orderby' => 'meta_value_num',
+                    'order' => 'ASC',
+                    'meta_query' => array(
+                        array(
+                            'key' => 'event_date',
+                            'compare'=> '>=',
+                            'value' => $today,
+                            'type' => 'numeric'
+                        ),
+                        array(
+                            'key'=>'related_programs',
+                            'compare'=> 'LIKE',
+                            'value' => '"' . get_the_ID() . '"'
+                        )
+                    )
+                )
+            );
+            if ($relatedEvents->have_posts()){
+	            echo '<hr class="section-break">';
+	            echo '<h2 class="headline headline--medium">Upcoming ' . get_the_title() . ' Events</h2>';
+	            //display events related to the current program
+	            while($relatedEvents->have_posts()){
+		            $relatedEvents->the_post();?>
+                    <div class="event-summary">
+                        <a class="event-summary__date t-center" href="#">
+                                <span class="event-summary__month">
+                                    <?php
+                                    $eventDate = new DateTime(get_field('event_date'));
+                                    echo $eventDate->format('M');
+                                    ?>
+
+                                </span>
+                            <span class="event-summary__day">
+                                    <?php
+                                    //$eventDate = new DateTime(get_field('event_date'));
+                                    echo $eventDate->format('d');
+                                    ?>
+                                </span>
+                        </a>
+                        <div class="event-summary__content">
+                            <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h5>
+                            <p>
+					            <?php if(has_excerpt()){
+						            echo get_the_excerpt();
+					            } else{
+						            echo wp_trim_words(get_the_content(),18);
+					            }
+					            ?>
+                                <a href="<?php the_permalink();?>" class="nu gray">Learn more</a>
+                            </p>
+                        </div>
+                    </div>
+		            <?php
+	            }
+            }
+
+	    ?>
+
+
     </div>
     <?php
 }
