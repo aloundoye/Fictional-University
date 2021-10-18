719dec1dd86880faae86b3fea5977861f6cc2eb0
Alassane Ndoye <aloutndoye.an@gmail.com>
2021-10-18 16:53:45 +0000

    Reduce duplicate code for all pages

--- wp-content/themes/fictional-university-theme/single.php
+++ wp-content/themes/fictional-university-theme/single.php
@@ -6,7 +6,7 @@
     pageBanner();
     ?>
 
-        
+
         <div class="container container--narrow page-section">
             <div class="metabox metabox--position-up metabox--with-home-link">
                 <p>
