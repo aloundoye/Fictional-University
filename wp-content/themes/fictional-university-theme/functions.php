4a876adadf635654c488b1f739acef90984ccbbb
Alassane Ndoye <aloutndoye.an@gmail.com>
2021-10-18 16:04:14 +0000

    Reduce duplicate code for the page banner

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -1,4 +1,33 @@
 <?php
+
+	function pageBanner($args= NULL){
+        if(!$args['title']){
+            $args['title'] = get_the_title();
+
+        }
+        if(!$args['subtitle']){
+            $args['subtitle'] = get_field('page_banner_subtitle');
+        }
+        if(!$args['photo']){
+            if(get_fields('page_banner_background_image') AND !is_archive() AND !is_home()){
+                $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
+            }else{
+                $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
+            }
+        }
+		?>
+		<div class="page-banner">
+			<div class="page-banner__bg-image"
+			     style="background-image: url(<?php echo $args['photo']; ?> )"></div>
+			<div class="page-banner__content container container--narrow">
+				<h1 class="page-banner__title"><?php echo $args['title']; ?></h1>
+				<div class="page-banner__intro">
+					<p><?php echo $args['subtitle']; ?></p>
+				</div>
+			</div>
+		</div>
+		<?php
+	}
 	//callback function for adding assets on the web site
 	function assets_files(){
 		wp_enqueue_script('main-university-js', get_theme_file_uri('/js/script-bundled.js'), NULL,
