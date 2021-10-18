182e55b37c692e490bc48ff4af7758cab9ec19c7
Alassane Ndoye <aloutndoye.an@gmail.com>
2021-10-18 13:22:47 +0000

    Dynamic backgroung Image for professor pages

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -15,6 +15,7 @@
 		add_theme_support('post-thumbnails');
 		add_image_size('professorLandscape', 400, 260, true);
 		add_image_size('professorPortrait', 480, 650, true);
+		add_image_size('pageBanner', 1500, 350, true);
 	}
 
 	add_action('after_setup_theme', 'university_features');
